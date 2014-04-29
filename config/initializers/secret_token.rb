d3fc9a15e785758ec0ab646ab19dfbe97332a72d
unknown <ccushma2@A20904.bcis.bates.edu>
2014-04-29 19:50:15 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hellobates::Application.config.secret_key_base = 'db627b3e2b4f9f43f3b1665e9538f9d47f6695d032cc3f42368d7f60663be127931e42d9cb3baa4f4eb77b685540835abf2a28da80767115f397ae5f571113bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hellobates::Application.config.session_store :cookie_store, key: '_hellobates_session'
